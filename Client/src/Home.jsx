bb4e948d943a4e30bb7a6596c24f85bfbe9093ad
akhi005 <kohinooraktherakhi5539@gmail.com>
2025-06-24 22:29:54 +0600

    modify home page

--- Client/src/Home.jsx
+++ Client/src/Home.jsx
@@ -2,27 +2,33 @@ import { Outlet } from "react-router-dom"
 
 export default function Home() {
   return (
-    <div className="min-h-screen w-full bg-gray-200">
-      <h2 className="text-7xl font-bold text-center pt-5">TalkTrack</h2>
-      <p className="px-72 text-center text-xl my-8">
-        A collaborative roadmap platform that empowers users to engage directly
-        with product development ideas through upvotes, comments, and
-        discussions. Designed for transparency and community-driven feedback.
-      </p>
-      <div className="flex  w-full px-14">
-        <div className="flex flex-col lg:flex-row items-center lg:items-stretch justify-center w-full ">
-          <div className="w-full flex justify-center items-center">
+    <div className="w-full bg-gray-100 min-h-screen"> 
+      <header className="py-10"> 
+        <h1 className="text-5xl md:text-6xl lg:text-7xl font-extrabold text-center text-gray-800 tracking-tight"> 
+          TalkTrack
+        </h1>
+        <p className="px-6 md:px-20 lg:px-72 text-center text-lg md:text-xl my-6 text-gray-600 leading-relaxed"> 
+          A collaborative roadmap platform that empowers users to engage directly
+          with product development ideas through upvotes, comments, and
+          discussions. Designed for transparency and community-driven feedback.
+        </p>
+      </header>
+
+      <main className="flex flex-col lg:flex-row items-center justify-center lg:px-14 pb-10"> 
+        <div className="flex flex-col lg:flex-row items-center lg:items-stretch justify-center w-full max-w-7xl mx-auto">
+          <div className="w-full lg:w-1/2"> 
             <img
               src="https://i.ibb.co/4wZWqKwN/road-map-removebg-preview.png"
-              className="w-full h-auto object-cover lg:max-h-screen rounded-xl"
-              alt="roadmap"
+              className="w-full h-full" 
+              alt="Roadmap" 
             />
           </div>
-          <div className="w-[800px] mr-12 flex items-center justify-center">
+
+          <div className="w-full lg:w-1/2 flex items-center justify-center p-4 lg:ml-12 lg:min-h-[500px]"> 
             <Outlet />
           </div>
-      </div>
-      </div>
+        </div>
+      </main>
     </div>
   )
 }
